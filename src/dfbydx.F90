37d25c9eda7f62a0356f98e1496bbfccdf6dfc13
Ashutosh Londhe (EXT) <asl@kos.hpsg.dcs.warwick.ac.uk>
2023-02-17 12:47:31 +0000

    test commit

--- src/dfbydx.F90
+++ src/dfbydx.F90
@@ -60,62 +60,23 @@ use OPS_Fortran_Reference
 !   ======
 !   EXPLICIT 4TH,4TH,4TH,6TH,8TH ORDER BOUNDARY TREATMENT
 
-        rangexyz = (/1,1,1,nyglbl,1,nzglbl/)
-        call ops_par_loop(dfbydx_kernel_lhpoint_4th_onesided, "dfbydx_lh_4th_onesided", senga_grid, 3, rangexyz,  &
-                          ops_arg_dat(functn, 1, s3d_000_to_p400_x, "real(8)", OPS_READ),  &
-                          ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE))
-
-        rangexyz = (/2,2,1,nyglbl,1,nzglbl/)
-        call ops_par_loop(dfbydx_kernel_lhpoint_4th_mixed, "dfbydx_lh_4th_mixed", senga_grid, 3, rangexyz,  &
-                          ops_arg_dat(functn, 1, s3d_p300_to_m100_x, "real(8)", OPS_READ),  &
-                          ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE))
-
-        rangexyz = (/3,3,1,nyglbl,1,nzglbl/)
-        call ops_par_loop(dfbydx_kernel_lhpoint_4th_centered, "dfbydx_lh_4th_centered", senga_grid, 3, rangexyz,  &
-                          ops_arg_dat(functn, 1, s3d_p200_to_m200_x_no000, "real(8)", OPS_READ),  &
-                          ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE))
-
-        rangexyz = (/4,4,1,nyglbl,1,nzglbl/)
-        call ops_par_loop(dfbydx_kernel_lhpoint_6th_centered, "dfbydx_lh_6th_centered", senga_grid, 3, rangexyz,  &
-                          ops_arg_dat(functn, 1, s3d_p300_to_m300_x_no000, "real(8)", OPS_READ),  &
-                          ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE))
-
-        rangexyz = (/5,5,1,nyglbl,1,nzglbl/)
-        call ops_par_loop(dfbydx_kernel_lhpoint_8th_centered, "dfbydx_lh_8th_centered", senga_grid, 3, rangexyz,  &
-                          ops_arg_dat(functn, 1, s3d_p400_to_m400_x_no000, "real(8)", OPS_READ),  &
-                          ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE))
-
+        rangexyz = (/1,5,1,nyglbl,1,nzglbl/)
+        call ops_par_loop(dfbydx_kernel_lhpoint, "dfbydx_lh", senga_grid, 3, rangexyz,  &
+                        ops_arg_dat(functn, 1, s3d_p400_to_m400_x, "real(8)", OPS_READ),  &
+                        ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE), &
+                        ops_arg_idx())
 !   =========================================================================
 
 !   RH END
 !   ======
 !       EXPLICIT 4TH,4TH,4TH,6TH,8TH ORDER BOUNDARY TREATMENT
 
-        rangexyz = (/nxglbl-4,nxglbl-4,1,nyglbl,1,nzglbl/)
-        call ops_par_loop(dfbydx_kernel_rhpoint_8th_centered, "dfbydx_rh_8th_centered", senga_grid, 3, rangexyz,  &
-                          ops_arg_dat(functn, 1, s3d_p400_to_m400_x_no000, "real(8)", OPS_READ),  &
-                          ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE))
-
-        rangexyz = (/nxglbl-3,nxglbl-3,1,nyglbl,1,nzglbl/)
-        call ops_par_loop(dfbydx_kernel_rhpoint_6th_centered, "dfbydx_rh_6th_centered", senga_grid, 3, rangexyz,  &
-                          ops_arg_dat(functn, 1, s3d_p300_to_m300_x_no000, "real(8)", OPS_READ),  &
-                          ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE))
-
-        rangexyz = (/nxglbl-2,nxglbl-2,1,nyglbl,1,nzglbl/)
-        call ops_par_loop(dfbydx_kernel_rhpoint_4th_centered, "dfbydx_rh_4th_centered", senga_grid, 3, rangexyz,  &
-                          ops_arg_dat(functn, 1, s3d_p200_to_m200_x_no000, "real(8)", OPS_READ),  &
-                          ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE))
-
-        rangexyz = (/nxglbl-1,nxglbl-1,1,nyglbl,1,nzglbl/)
-        call ops_par_loop(dfbydx_kernel_rhpoint_4th_mixed, "dfbydx_rh_4th_mixed", senga_grid, 3, rangexyz,  &
-                          ops_arg_dat(functn, 1, s3d_p100_to_m300_x, "real(8)", OPS_READ),  &
-                          ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE))
-
-        rangexyz = (/nxglbl,nxglbl,1,nyglbl,1,nzglbl/)
-        call ops_par_loop(dfbydx_kernel_rhpoint_4th_onesided, "dfbydx_rh_4th_onesided", senga_grid, 3, rangexyz,  &
-                          ops_arg_dat(functn, 1, s3d_000_to_m400_x, "real(8)", OPS_READ),  &
-                          ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE))
-
+        rangexyz = (/nxglbl-4,nxglbl,1,nyglbl,1,nzglbl/)
+        call ops_par_loop(dfbydx_kernel_rhpoint, "dfbydx_rh", senga_grid, 3, rangexyz,  &
+                        ops_arg_dat(functn, 1, s3d_p400_to_m400_x, "real(8)", OPS_READ),  &
+                        ops_arg_dat(fderiv, 1, s3d_000, "real(8)", OPS_WRITE), &
+                        ops_arg_gbl(nxglbl, 1, "integer", OPS_READ), &
+                        ops_arg_idx())
 !   =========================================================================
 
 !   SCALING
