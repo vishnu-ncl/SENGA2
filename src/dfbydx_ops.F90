8c03c674ad42c3d528d2cc6bf1c9571fd3bc08ef
not populated <londhe.ashutosh@gmail.com>
2024-04-21 01:17:09 +0100

    pushing created ops file for dfbydx

---
+++ src/dfbydx_ops.F90
@@ -0,0 +1,67 @@
+
+! Auto-generated at 2024-04-21 01:16:15.986984 by ops-translator
+SUBROUTINE dfbydx(functn, fderiv)
+
+  USE ops_fortran_declarations
+  USE ops_fortran_rt_support
+  USE dfbydx_kernel_main_module
+
+  USE OPS_CONSTANTS
+  USE, INTRINSIC :: ISO_C_BINDING
+
+  USE com_senga
+  USE com_ops_senga
+
+  !   *************************************************************************
+
+  !   DFBYDX
+  !   ======
+
+  !   AUTHOR
+  !   ------
+  !   R.S.CANT
+
+  !   CHANGE RECORD
+  !   -------------
+  !   01-AUG-1996:  CREATED
+  !   28-MAR-2003:  RSC MODIFIED FOR SENGA2
+
+  !   DESCRIPTION
+  !   -----------
+  !   DNS CODE SENGA2
+  !   EVALUATES FIRST X-DERIVATIVE OF SPECIFIED FUNCTION
+  !   EXPLICIT 10TH ORDER FINITE DIFFERENCE METHOD
+  !   EXPLICIT 8TH,6TH,4TH,4TH ORDER END CONDITIONS
+
+  !   *************************************************************************
+
+  !   ARGUMENTS
+  !   =========
+  TYPE(ops_dat) :: functn, fderiv
+
+  !   LOCAL DATA
+  !   ==========
+  INTEGER(KIND = 4) :: rangexyz(6)
+
+  !   BEGIN
+  !   =====
+
+  !   =========================================================================
+
+  !   INTERIOR SCHEME
+  !   ===============
+
+  !   TENTH ORDER EXPLICIT DIFFERENCES
+  rangexyz = [1, nxglbl, 1, nyglbl, 1, nzglbl]
+  CALL dfbydx_kernel_main_host("dfbydx_main_scheme", senga_grid, 3, rangexyz, &
+ops_arg_dat(functn, 1, s3d_p500_to_m500_x, "real(kind=8)", OPS_READ), &
+ops_arg_dat(fderiv, 1, s3d_000, "real(kind=8)", OPS_WRITE), &
+ops_arg_gbl(nxglbl, 1, "integer(kind=4)", OPS_READ), &
+ops_arg_gbl(nendxl, 1, "integer(kind=4)", OPS_READ), &
+ops_arg_gbl(nendxr, 1, "integer(kind=4)", OPS_READ), &
+ops_arg_gbl(nbound, 1, "integer(kind=4)", OPS_READ), &
+ops_arg_idx())
+
+  !   =========================================================================
+
+END SUBROUTINE dfbydx
