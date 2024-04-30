694bbf43d54ff6ae7156a3c32e6b08ff5761acc6
user name <ashutoshl@cirrus-login2.ib0.icexa.epcc.ed.ac.uk>
2024-04-30 22:35:56 +0100

    updated post processing script

--- post/src_visual/chmech.f95
+++ post/src_visual/chmech.f95
@@ -20,9 +20,23 @@ implicit none
 
 wrtspc(1)%wrtdst = "R"
 wrtspc(2)%wrtdst = "P"
-                                       
+wrtspc(3)%wrtdst = "Y1"
+wrtspc(4)%wrtdst = "Y2"
+wrtspc(5)%wrtdst = "Y3"
+wrtspc(6)%wrtdst = "Y4"
+wrtspc(7)%wrtdst = "Y5"
+wrtspc(8)%wrtdst = "Y6"
+wrtspc(9)%wrtdst = "Y7"
+
 wrtrrt(1)%wrtdst = "RRTE_R"
 wrtrrt(2)%wrtdst = "RRTE_P"
+wrtrrt(3)%wrtdst = "RRTE_Y1"
+wrtrrt(4)%wrtdst = "RRTE_Y2"
+wrtrrt(5)%wrtdst = "RRTE_Y3"
+wrtrrt(6)%wrtdst = "RRTE_Y4"
+wrtrrt(7)%wrtdst = "RRTE_Y5"
+wrtrrt(8)%wrtdst = "RRTE_Y6"
+wrtrrt(9)%wrtdst = "RRTE_Y7"
 
 end subroutine set_mech_tags
 
