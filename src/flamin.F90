a8a6ae2c50e2f40b87d6f698792bf66612f58cf3
vishnu-ncl <vishnu.mohan@newcastle.ac.uk>
2024-06-19 15:53:40 +0100

    adding flamin.F90 changes

--- src/flamin.F90
+++ src/flamin.F90
@@ -30,7 +30,7 @@ SUBROUTINE flamin
 !     GLOBAL DATA
 !     ===========
 !     -------------------------------------------------------------------------
-
+use data_types
 use com_senga
 !     -------------------------------------------------------------------------
 
@@ -38,31 +38,30 @@ use com_senga
 !     PARAMETERS
 !     ==========
 !     ESTIMATED FLAME LOCATION AND THICKNESS
-REAL(kind=8) :: clocat,cthick
-PARAMETER(clocat = 0.0025_8, cthick = 0.0005_8)
+real(kind=dp) :: clocat,cthick
+PARAMETER(clocat = 0.0025_dp, cthick = 0.0005_dp)
 
 !C     PINCH OF HYDROGEN ATOM
-!      REAL(kind=8) HPINCH,HLOCAT,HTHICK
+!      real(kind=dp) HPINCH,HLOCAT,HTHICK
 !      PARAMETER(HPINCH = 1.0D-10, HLOCAT = 2.5D-3, HTHICK = 1.0D-4)
 !C     PINCH OF HYDROGEN MOLECULE
-!      REAL(kind=8) H2PNCH,H2LOCT,H2THCK
+!      real(kind=dp) H2PNCH,H2LOCT,H2THCK
 !      PARAMETER(H2PNCH = 1.0D-6, H2LOCT = 2.5D-3, H2THCK = 2.5D-4)
 
 
 !     FUNCTION
 !     ========
-REAL(kind=8) :: erfunc
+real(kind=dp) :: erfunc
 EXTERNAL erfunc
 
 
 !     LOCAL DATA
 !     ==========
-REAL(kind=8) :: crin(1:nxsize)
-REAL(kind=8) :: yrinr(nspcmx),yrinp(nspcmx)
-REAL(kind=8) :: trinr,trinp
-REAL(kind=8) :: deltag,xcoord,argmnt
-REAL(kind=8) :: flxmas
-REAL(kind=8) :: xrgmnt,yrgmnt,zrgmnt
+real(kind=dp) :: crin(1:nxsize)
+real(kind=dp) :: yrinr(nspcmx),yrinp(nspcmx)
+real(kind=dp) :: trinr,trinp
+real(kind=dp) :: deltag,xcoord,argmnt
+real(kind=dp) :: flxmas
 INTEGER :: icproc
 INTEGER :: igofst
 INTEGER :: ix
@@ -83,97 +82,240 @@ INTEGER :: ispec
 !     -----------------------
 !     REACTANT TEMPERATURE SET IN CONTROL FILE
 trinr = trin
-u0 = 34.789806_8
-
-!Mixture gas constant
-rglocl = zero
-do jspec = 1,nspec
-  rglocl = rglocl + rgspec(jspec)*yrin(jspec)
-end do
-!Times (constant) density
-rglocl  = drin*rglocl
-
-!Global indexing
-!---------------
-deltagx = xgdlen/(REAL(nxglbl-1,kind=8))
-deltagy = ygdlen/(REAL(nyglbl-1,kind=8))
-deltagz = zgdlen/(REAL(nzglbl-1,kind=8))
-
-igofstx = 0
-do icproc = 0, ixproc-1
-  igofstx = igofstx + npmapx(icproc)
-end do
-
-igofsty = 0
-do icproc = 0, iyproc-1
-  igofsty = igofsty + npmapy(icproc)
-end do
-
-igofstz = 0
-do icproc = 0, izproc-1
-  igofstz = igofstz + npmapz(icproc)
-end do
-
-!SET THE VELOCITY PROFILE FOR TGV
-!--------------------------------
-angfrx = 8.0_8*atan(1.0_8)/xgdlen
-angfry = 8.0_8*atan(1.0_8)/ygdlen
-angfrz = 8.0_8*atan(1.0_8)/zgdlen
-
-do kc = kstal,kstol
- do jc = jstal,jstol
-  do ic = istal,istol
-
-   ix = igofstx + ic
-   iy = igofsty + jc
-   iz = igofstz + kc
-
-   xcoord = REAL(ix-1,kind=8)*deltagx
-   ycoord = REAL(iy-1,kind=8)*deltagy
-   zcoord = REAL(iz-1,kind=8)*deltagz
-
-   xrgmnt = angfrx*xcoord
-   yrgmnt = angfry*ycoord
-   zrgmnt = angfrz*zcoord
-
-!       set taylor-green vortex velocity field
-   urun(ic,jc,kc)=u0*dsin(xrgmnt)*dcos(yrgmnt)*dcos(zrgmnt)
-   vrun(ic,jc,kc)=-u0*dcos(xrgmnt)*dsin(yrgmnt)*dcos(zrgmnt)
-   wrun(ic,jc,kc)=0.0_8
-!       set pressure profile assuming constant density
-   xrgmnt = 2.0_8*xrgmnt
-   yrgmnt = 2.0_8*yrgmnt
-   zrgmnt = 2.0_8*zrgmnt
-
-   prun(ic,jc,kc)=prin+((drin*u0*u0)/16.0_8)*(dcos(xrgmnt)+dcos(yrgmnt))*(dcos(zrgmnt)+2.0_8)
-  end do
- end do
-end do
-
-!set temperature profile assuming constant density
-
-do kc = kstal,kstol
-  do jc = jstal,jstol
-    do ic = istal,istol
-
-      trun(ic,jc,kc) = prun(ic,jc,kc)/rglocl
-
-    end do
-  end do
-end do
+!      TRINP = 2330.96554
+trinp = 1400.0_dp
 
 
-!set constant density
+!     SET SPECIES MASS FRACTIONS
+!     --------------------------
+!     OVERRIDE MASS FRACTION VALUES SET IN CONTROL FILE
 
-do kc = kstal,kstol
-  do jc = jstal,jstol
-    do ic = istal,istol
+!     REACTANTS
+yrinr(1) = 0.0116075_dp     !2.8312571D-2
+yrinr(2) = 0.2302951_dp     !2.26500566D-1
+DO ispec = 3,nspm1
+  yrinr(ispec) = zero
+END DO
 
-      drun(ic,jc,kc) = drin
+yrinr(nspec) = zero
+DO ispec = 1,nspm1
+  yrinr(nspec) = yrinr(nspec) + yrinr(ispec)
+END DO
+yrinr(nspec) = one - yrinr(nspec)
 
-    end do
-  end do
-end do
+!     PRODUCTS
+yrinp(1) = zero
+yrinp(2) = 0.1390901_dp     !ZERO
+yrinp(3) = 0.1043176_dp     !2.54716981D-1
+DO ispec = 4,nspm1
+  yrinp(ispec) = zero
+END DO
+yrinp(nspec) = zero
+DO ispec = 1,nspm1
+  yrinp(nspec) = yrinp(nspec) + yrinp(ispec)
+END DO
+yrinp(nspec) = one - yrinp(nspec)
+
+!     WRITE TO REPORT FILE
+IF(iproc == 0)THEN
+  
+!        OPEN(UNIT=NCREPT,FILE=FNREPT,STATUS='OLD',FORM='FORMATTED')
+  
+!C       GO TO EOF
+!1000    CONTINUE
+!          READ(NCREPT,9000,END=1010)
+!          GOTO 1000
+!1010    BACKSPACE(NCREPT)
+  
+  WRITE(ncrept,*)
+  WRITE(ncrept,*)'FLAMIN: reactant mass fractions:'
+  DO ispec = 1,nspec
+    WRITE(ncrept,'(I5,1PE15.7)')ispec,yrinr(ispec)
+  END DO
+  WRITE(ncrept,*)
+  
+  WRITE(ncrept,*)'FLAMIN: product mass fractions:'
+  DO ispec = 1,nspec
+    WRITE(ncrept,'(I5,1PE15.7)')ispec,yrinp(ispec)
+  END DO
+  WRITE(ncrept,*)
+  
+  WRITE(ncrept,*)'FLAMIN: reactant and product temperatures:'
+  WRITE(ncrept,'(2(1PE15.7))')trinr,trinp
+  WRITE(ncrept,*)
+  
+!        CLOSE(NCREPT)
+  
+END IF
+
+
+!     GLOBAL INDEXING
+!     ---------------
+deltag = xgdlen/(REAL(nxglbl-1,kind=dp))
+
+igofst = 0
+DO icproc = 0, ixproc-1
+  igofst = igofst + npmapx(icproc)
+END DO
+
+
+!     SET REACTION PROGRESS VARIABLE PROFILE
+!     --------------------------------------
+!     SIMPLE 1D LEFT-FACING ERROR FUNCTION PROFILE
+DO ic = istal,istol
+  
+  ix = igofst + ic
+  xcoord = REAL(ix-1,kind=dp)*deltag
+  argmnt = (xcoord-clocat)/cthick
+  crin(ic) = half*(one+erfunc(argmnt))
+  
+END DO
+
+!C     SIMPLE 1D RIGHT-FACING ERROR FUNCTION PROFILE
+!      DO IC = ISTAL,ISTOL
+
+!        IX = IGOFST + IC
+!        XCOORD = REAL(IX-1)*DELTAG
+!        ARGMNT = (XCOORD-CLOCAT)/CTHICK
+!        CRIN(IC) = HALF*(ONE+ERFUNC(-ARGMNT))
+
+!      ENDDO
+
+
+!     SET SPECIES MASS FRACTION PROFILES
+!     ----------------------------------
+DO ispec = 1, nspm1
+  
+  DO kc = kstal,kstol
+    DO jc = jstal,jstol
+      DO ic = istal,istol
+        
+        yrun(ic,jc,kc,ispec) = yrinr(ispec)  &
+            + crin(ic)*(yrinp(ispec) - yrinr(ispec))
+        
+      END DO
+    END DO
+  END DO
+  
+END DO
+
+!C     SG 25-STEP MECHANISM
+!C     ADD A PINCH OF HYDROGEN ATOM
+!C     AND HYDROGEN MOLECULE
+!      DO KC = KSTAL,KSTOL
+!        DO JC = JSTAL,JSTOL
+!          DO IC = ISTAL,ISTOL
+
+!            IX = IGOFST + IC
+!            XCOORD = REAL(IX-1)*DELTAG
+!            ARGMNT = (XCOORD-H2LOCT)/H2THCK
+!            YRUN(IC,JC,KC,5) = H2PNCH*EXP(-ARGMNT*ARGMNT)
+!            ARGMNT = (XCOORD-HLOCAT)/HTHICK
+!            YRUN(IC,JC,KC,8) = HPINCH*EXP(-ARGMNT*ARGMNT)
+
+!          ENDDO
+!        ENDDO
+!      ENDDO
+
+DO kc = kstal,kstol
+  DO jc = jstal,jstol
+    DO ic = istal,istol
+      
+      yrun(ic,jc,kc,nspec) = zero
+      
+    END DO
+  END DO
+END DO
+
+DO ispec = 1, nspm1
+  DO kc = kstal,kstol
+    DO jc = jstal,jstol
+      DO ic = istal,istol
+        
+        yrun(ic,jc,kc,nspec) = yrun(ic,jc,kc,nspec) + yrun(ic,jc,kc,ispec)
+        
+      END DO
+    END DO
+  END DO
+END DO
+
+DO kc = kstal,kstol
+  DO jc = jstal,jstol
+    DO ic = istal,istol
+      
+      yrun(ic,jc,kc,nspec) = one - yrun(ic,jc,kc,nspec)
+      
+    END DO
+  END DO
+END DO
+
+
+!     SET TEMPERATURE PROFILE
+!     -----------------------
+DO kc = kstal,kstol
+  DO jc = jstal,jstol
+    DO ic = istal,istol
+      
+      trun(ic,jc,kc) = trinr +  crin(ic)*(trinp - trinr)
+      
+    END DO
+  END DO
+END DO
+
+
+!     SET DENSITY PROFILE ASSUMING CONSTANT PRESSURE
+!     -------------------
+!     PRESSURE SET IN CONTROL FILE
+
+DO kc = kstal,kstol
+  DO jc = jstal,jstol
+    DO ic = istal,istol
+      
+      store1(ic,jc,kc) = zero
+      
+    END DO
+  END DO
+END DO
+
+DO ispec = 1,nspec
+  DO kc = kstal,kstol
+    DO jc = jstal,jstol
+      DO ic = istal,istol
+        
+        store1(ic,jc,kc) = store1(ic,jc,kc)  &
+            + rgspec(ispec)*yrun(ic,jc,kc,ispec)
+        
+      END DO
+    END DO
+  END DO
+END DO
+
+DO kc = kstal,kstol
+  DO jc = jstal,jstol
+    DO ic = istal,istol
+      
+      drun(ic,jc,kc) = prin/(store1(ic,jc,kc)*trun(ic,jc,kc))
+      
+    END DO
+  END DO
+END DO
+
+
+!     SET VELOCITY PROFILE ASSUMING CONSTANT MASS FLUX
+!     --------------------
+!     INITIAL (INLET) VEOCITY SET IN CONTROL FILE
+flxmas = drin*urin
+DO kc = kstal,kstol
+  DO jc = jstal,jstol
+    DO ic = istal,istol
+      
+      urun(ic,jc,kc) = flxmas/drun(ic,jc,kc)
+      
+    END DO
+  END DO
+END DO
+
+!     =========================================================================
 
 
 RETURN
